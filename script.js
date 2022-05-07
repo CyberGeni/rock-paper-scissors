6e01619a97f8a16d2354a02169eddda486eedbe8
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-05-07 01:23:09 +0100

    basic gameplay

---
+++ script.js
@@ -0,0 +1,28 @@
+// computer gameplay
+function computerPlay() {
+    choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+computerPlay()
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It\s a tie!';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose, Paper beats rock!';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win, Rock beats scissors!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win, Paper beats rock!';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose, Scissors beats Paper!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose, Rock beats Scissors!';
+    } else {
+        return 'You win, Scissors beats Paper!';
+    }
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
