d53347c45712db2f63ee0a6a4f0dbe9251704a3c
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-07-22 16:40:32 +0100

    added final player and computer scores display

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let playerScore = 0
+let computerScore = 0
+
 // computer gameplay
 function computerPlay() {
     choices = ['rock', 'paper', 'scissors'];
@@ -7,26 +11,32 @@ computerPlay()
 
 // human gameplay
 function playRound(playerSelection, computerSelection) {
+    
     if (playerSelection === computerSelection) {
         return 'It\s a tie!';
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
         return 'You lose, Paper beats rock!';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
         return 'You win, Rock beats scissors!';
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
         return 'You win, Paper beats rock!';
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
         return 'You lose, Scissors beats Paper!';
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++;
         return 'You lose, Rock beats Scissors!';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        playerScore++;
         return 'You win, Scissors beats Paper!';
     } else {
         return 'Invalid input!';
     }
 }
 // using do/while loop 
-
 
 // rounds = 0
 // do {
@@ -37,8 +47,13 @@ function playRound(playerSelection, computerSelection) {
 // }
 // while (rounds < 5)
 
-for (let rounds = 0; rounds < 5; rounds++) {
-    const playerSelection = prompt('Rock, Paper or Scissors? ');
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
- }
+function game () {
+    for (let rounds = 0; rounds < 5; rounds++) {
+        const playerSelection = prompt('Rock, Paper or Scissors? ');
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+}
+game()
+
